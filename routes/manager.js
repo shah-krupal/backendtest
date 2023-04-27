7ee0ea2eb1e5f09b349deea49dc08fca810ac251
shah-krupal <77288288+shah-krupal@users.noreply.github.com>
2023-04-27 15:52:37 +0530

    commit

--- routes/manager.js
+++ routes/manager.js
@@ -15,6 +15,7 @@ router.post('/', async (req,res)=>{                                   // Create 
         newmanager.ManagerID = query.NoOfManager;
         const dept = await Department.findOne({DepartmentID:newmanager.Department});
         newmanager.DepartmentName = dept.DepartmentName;
+        newmanager.DateOfJoining = Date.now();
         const result = await Manager.create(newmanager);
         res.status(200).json({"success":"true"}) ;
     }catch(err){
